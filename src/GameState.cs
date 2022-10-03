31dcb0cea6cf732b0834dc8941fed63843eaaa53
Mitchell Fenner <mitchfen@protonmail.com>
2022-10-02 22:39:32 -0400

    feat: implement GameState class (#135)

---
+++ src/GameState.cs
@@ -0,0 +1,16 @@
+namespace mitchfenxyz;
+
+public enum Location { clearing, house, forest }
+
+public class GameState
+{
+    public List<string> InputCommands { get; set; }
+
+    public Location PlayerLocation { get; set; }
+
+    public GameState()
+    {
+        this.PlayerLocation = Location.clearing;
+        InputCommands = new List<string>();
+    }
+}
