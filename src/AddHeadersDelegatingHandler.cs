2fbbeb6517dba7511c9493f71d13e876254fef13
Mitchell Fenner <mfenner@configit.com>
2025-01-27 11:24:58 -0500

    try add security headers

---
+++ src/AddHeadersDelegatingHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace MitchfenSite;
+
+public class AddHeadersDelegatingHandler() : DelegatingHandler(new HttpClientHandler())
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+        request.Headers.Add("Content-Security-Policy", "default-src 'self'");
+        request.Headers.Add("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
