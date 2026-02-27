45a532c9ab2e8ff7273bf38b4b3981ea6a30de83
Mitch Fenner <mitch@mitchfen.com>
2026-02-26 19:34:13 -0500

    little improvements

--- src/Components/Terminal.razor.Commands.cs
+++ src/Components/Terminal.razor.Commands.cs
@@ -7,20 +7,24 @@ public partial class Terminal
         switch (cmd)
         {
             case "help":
-                OutputLines.Add("<span class='orange'>Available commands:</span>");
-                OutputLines.Add("  <span class='cyan'>about</span>");
-                OutputLines.Add("  <span class='cyan'>projects</span>");
-                OutputLines.Add("  <span class='cyan'>socials</span>");
-                OutputLines.Add("  <span class='cyan'>clear</span>");
+                OutputLines.Add("  <span class='cyan'>about</span> - get info about me");
+                OutputLines.Add("  <span class='cyan'>projects</span> - see what I'm working on");
+                OutputLines.Add("  <span class='cyan'>socials</span> - find me elsewhere online");
+                OutputLines.Add("  <span class='cyan'>clear</span> - clear the screen");
                 break;
             
             case "about":
-                OutputLines.Add("<span class='orange'>About me:</span>");
-                OutputLines.Add("I'm <span class='green'>Mitchell Fenner</span>, a Senior Cloud Operations Engineer, homelab enthusiast, and avid rock climber.");
+                OutputLines.Add("I'm <span class='green'>Mitchell Fenner</span>, a Senior Cloud Operations Engineer and homelab enthusiast.");
                 break;
 
+            case "welcome":
+                OutputLines.Add("<span class='green'>Welcome to Mitchell Fenner's personal website 🤗</span>");
+                OutputLines.Add("<span class='comment'>Hint: type help to view commands</span>");
+                break;
+
+
+
             case "projects":
-                OutputLines.Add("<span class='orange'>My projects:</span>");
                 foreach (var project in Projects)
                 {
                     if (project.IsRedacted)
@@ -39,9 +43,9 @@ public partial class Terminal
                 break;
             
             case "socials":
-                OutputLines.Add("<span class='orange'>Social links:</span>");
                 OutputLines.Add("• <a href='https://github.com/mitchfen' target='_blank'>GitHub</a>");
                 OutputLines.Add("• <a href='https://linkedin.com/in/mitchfen' target='_blank'>LinkedIn</a>");
+                OutputLines.Add("• <a href='mailto:mitch@mitchfen.com' target='_blank'>Send me an email</a>");
                 break;
 
             case "clear":
