4b953c2b20bf1822903c22fb627c8076da1245e5
Mitchell Fenner <mitch@mitchfen.com>
2026-06-19 13:28:15 -0400

    fiddly update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './output/wwwroot',
-  compressHTML: true
+  compressHTML: true,
+  site: 'https://mitchfen.com',
+  prefetch: true,
+  trailingSlash: 'never',
+  build: {
+    format: 'file'
+  },
+  integrations: [sitemap()]
 });
